a356921f62cd5f0aa31a8e8390e3df3cebdfa011
UnusualRuWei <moradafranzruie@gmail.com>
2024-06-03 13:39:40 +0800

    Cleaning

--- app/src/main/java/com/intprog/woodablesapp/AdminAssesmentFragment.kt
+++ app/src/main/java/com/intprog/woodablesapp/AdminAssesmentFragment.kt
@@ -79,7 +79,6 @@ class AdminAssesmentFragment : Fragment() {
                         val location = document.getString("location")
                         val email = document.getString("email")
                         val status = document.getString("status") ?: ""
-                        Toast.makeText(context, status.toString(), Toast.LENGTH_SHORT).show()
                         addDocumentToLayout(
                             documentId,
                             fullName,
